2f9126934b77f7b03afac16ab4663396da9d833f
ELCOT <ELCOT@192.168.1.6>
2020-06-13 20:54:10 +0530

    Adding this comment to check auto triggering of build in jenkins

--- Calculator/src/com/tharic/Tester.java
+++ Calculator/src/com/tharic/Tester.java
@@ -9,6 +9,7 @@ public class Tester {
 		System.out.println(c.sub(34,12));
 		System.out.println(c.multiply(5,6));
 		System.out.println(c.divide(343,7));
+		// Adding this comment to check auto triggering of build in jenkins
 	}
 
 }
