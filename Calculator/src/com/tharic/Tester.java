6b6ca685e4508fd7b7fb18eecd3eac44dfc93b57
ELCOT <ELCOT@192.168.1.6>
2020-06-13 20:57:40 +0530

    // Adding this comment to check auto triggering of build in jenkins

--- Calculator/src/com/tharic/Tester.java
+++ Calculator/src/com/tharic/Tester.java
@@ -3,13 +3,13 @@ package com.tharic;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Adding this comment to check auto triggering of build in jenkins
 		Calculator c=new Calculator();
 		System.out.println(c.add(3,5));
 		System.out.println(c.sub(34,12));
 		System.out.println(c.multiply(5,6));
 		System.out.println(c.divide(343,7));
-		// Adding this comment to check auto triggering of build in jenkins
+		
 	}
 
 }
